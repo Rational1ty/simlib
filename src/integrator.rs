96fc07185b2304f9d33f283d070fd0052d9ac8b7
Matthew Davidson <mdavidson390@outlook.com>
2026-02-26 11:00:33 -0600

    Pass correct time to RK4 integrator stages

--- src/integrator.rs
+++ src/integrator.rs
@@ -14,24 +14,26 @@ pub fn runge_kutta_4<S, D, L, U>(
 {
 	// we want to pass in the current integration dt to the functions, which might be different from
 	// the simulation's default dt
-	let time = &SimTime { dt, ..*sim_time };
+	let t_0 = &SimTime { dt, ..*sim_time };
+	let t_half = &SimTime { t: sim_time.t + (0.5 * dt), dt, ..*sim_time };
+	let t_full = &SimTime { t: sim_time.t + dt, dt, ..*sim_time };
 
-	let state = state_loader(sim, time);
+	let state = state_loader(sim, t_0);
 	let n = state.len();
 
-	let k1 = derivative(sim, time);
+	let k1 = derivative(sim, t_0);
 
 	let y2: Vec<f64> = (0..n).map(|i| state[i] + 0.5 * dt * k1[i]).collect();
 	state_unloader(sim, &y2);
-	let k2 = derivative(sim, time);
+	let k2 = derivative(sim, t_half);
 
 	let y3: Vec<f64> = (0..n).map(|i| state[i] + 0.5 * dt * k2[i]).collect();
 	state_unloader(sim, &y3);
-	let k3 = derivative(sim, time);
+	let k3 = derivative(sim, t_half);
 
 	let y4: Vec<f64> = (0..n).map(|i| state[i] + dt * k3[i]).collect();
 	state_unloader(sim, &y4);
-	let k4 = derivative(sim, time);
+	let k4 = derivative(sim, t_full);
 
 	let res: Vec<f64> = (0..n)
 		.map(|i| state[i] + (dt / 6.0) * (k1[i] + 2.0 * k2[i] + 2.0 * k3[i] + k4[i]))
